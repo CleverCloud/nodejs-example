87b630e28424fc88856c8317eb2b170d48ede707
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-06-26 18:35:30 +0200

    Add logs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import http from 'http';
 
 const port = process.env.PORT || 8080;
-const server = http.createServer((req, res) => res.end(`Hello World!!!`));
+const server = http.createServer((req, res) => {
+  console.log(`REQUEST ${req.method} ${req.url}`);
+  return res.end(`Hello World!!!`);
+});
 server.listen(port);
 console.log(`HTTP server running at port ${port}`);
